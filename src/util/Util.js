10b948e95f0a51fa98fcd8f2b9ab06c5eff0e0a4
Jalal Addisu <jayaddisu@gmail.com>
2024-05-20 14:08:40 +0300

    Adding some files

--- src/util/Util.js
+++ src/util/Util.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: 'http://192.168.3.111:8000/',
 //   timeout: 5000, 
 //   headers: {
-//     'Content-Type': 'application/json',
+//     'Content-Type': 'application/json',gtit 
 //   },
 });
 
